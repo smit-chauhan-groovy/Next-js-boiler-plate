102da74809499c5a136739c0d9341dcbc4aeb080
smit.groovy <smit.chauhan@groovyweb.co>
2024-10-02 15:31:09 +0530

    Fix : issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  //   redirects: async () => {
+  //     return [
+  //       {
+  //         source: "/",
+  //         destination: "/about",
+  //         permanent: false, // Set to true if you want a permanent redirect (301)
+  //       },
+  //     ];
+  //   },
+};
 
 export default nextConfig;
